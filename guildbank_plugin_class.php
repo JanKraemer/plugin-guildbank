876418e0640a6c574188290ccbd04c9d36a6b423
GodMod <GodMod@users.noreply.github.com>
2015-02-10 09:23:44 +0100

    bumped version to 2.0.1

--- guildbank_plugin_class.php
+++ guildbank_plugin_class.php
@@ -25,7 +25,7 @@ if (!defined('EQDKP_INC')){
 
 class guildbank extends plugin_generic {
 	public $vstatus		= 'Beta';
-	public $version		= '2.0.0';
+	public $version		= '2.0.1';
 	public $copyright 	= 'Wallenium';
 	
 	protected static $apiLevel = 20;
