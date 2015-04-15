96943b9d0d4b20f23831fb8c7dfd22a153fe2105
Inkraja <Inkraja@arcor.de>
2015-04-15 19:18:57 +0200

    Add Infos for TSW

---
+++ games/tsw/money.config.php
@@ -0,0 +1,36 @@
+<?php
+/*	Project:	EQdkp-Plus
+ *	Package:	Guildbanker Plugin
+ *	Link:		http://eqdkp-plus.eu
+ *
+ *	Copyright (C) 2006-2015 EQdkp-Plus Developer Team
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU Affero General Public License as published
+ *	by the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU Affero General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Affero General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+	// The array with the images
+	$money_data = array(
+		'PAX'		=> array(
+			'icon'			=> array(
+				'type'		=> 'svg',
+				'name'=> $this->root_path.'plugins/guildbank/games/tsw/pax.png',
+			),
+			'factor'		=> 1,
+			'size'			=> 'unlimited',
+			'language'		=> $this->user->lang(array('gb_currency', 'pax')),
+			'short_lang'	=> $this->user->lang(array('gb_currency', 'pax_s')),
+		)
+	);
+
+ ?>
