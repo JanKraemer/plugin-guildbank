5297318e053e5f6d678ce621580e6c2cc7b8b2ea
Simon W <s.wallmann@kompsoft.de>
2015-06-22 13:01:56 +0200

    bump version

--- guildbank_plugin_class.php
+++ guildbank_plugin_class.php
@@ -25,7 +25,7 @@ if (!defined('EQDKP_INC')){
 
 class guildbank extends plugin_generic {
 	public $vstatus		= 'Stable';
-	public $version		= '2.1.5';
+	public $version		= '2.2.0';
 	public $copyright 	= 'Wallenium';
 	
 	protected static $apiLevel = 20;
